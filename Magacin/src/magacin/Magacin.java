a172486541e95c7aefbf8bc281e5e9737715e39f
leki0 <aleksastancic9@gmail.com>
2024-04-03 17:21:23 +0200

    Napravljena klasa Magacin koja implementira interfejs.

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import magacin.interfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal a, int kolicina) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(int kolicina) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
