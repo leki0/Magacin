6323708bcf505a98588b39e9cc18e6dfe17d93a5
leki0 <aleksastancic9@gmail.com>
2024-04-03 16:34:09 +0200

    Napravljene klase, privatni atributi i odgovarajuce get i set metode.

---
+++ Magacin/src/magacin/Artikal.java
@@ -0,0 +1,42 @@
+package magacin;
+
+public abstract class Artikal {
+
+	private String naziv;
+	private int sifra;
+	private int kolicina;
+	private String opis;
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public int getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+
+	public int getKolicina() {
+		return kolicina;
+	}
+
+	public void setKolicina(int kolicina) {
+		this.kolicina = kolicina;
+	}
+
+	public String getOpis() {
+		return opis;
+	}
+
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+
+}
